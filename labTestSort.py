0cc1563e92cd7d041f8fd9a9f1c4dd5fdcbb6df3
Пехтерев Иван Игоревич <kakoitachel576@gmail.com>
2026-05-18 21:29:53 +0300

    Add time counter

--- labTestSort.py
+++ labTestSort.py
@@ -1,4 +1,9 @@
+import time
+
+
 def bubble_sort(arr):
+    start_time = time.perf_counter()
+
     n = len(arr)
     for i in range(n):
         swapped = False
@@ -8,9 +13,16 @@ def bubble_sort(arr):
                 swapped = True
         if not swapped:
             break
+
+    end_time = time.perf_counter()
+    print(f"Bubble Sort выполнился за {end_time - start_time:.8f} секунд")
+
     return arr
 
+
 def insertion_sort(arr):
+    start_time = time.perf_counter()
+
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
@@ -21,8 +33,17 @@ def insertion_sort(arr):
 
         arr[j + 1] = key
 
+    end_time = time.perf_counter()
+    print(f"Insertion Sort выполнился за {end_time - start_time:.8f} секунд")
+
+    return arr
+
+
 numbers = [64, 34, 25, 12, 22, 11, 90]
 
 if __name__ == "__main__":
-    print("Отсортированный список пузырьком:", bubble_sort(numbers))
-    print("Отсортированный список пузырьком:", insertion_sort(numbers))
+    print("Отсортированный список пузырьком:",
+          bubble_sort(numbers.copy()))
+
+    print("Отсортированный список вставками:",
+          insertion_sort(numbers.copy()))
