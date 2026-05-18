c5e27d15d03930e5fd3f53e30f6d901da2f6fea2
Пехтерев Иван Игоревич <kakoitachel576@gmail.com>
2026-05-18 21:24:48 +0300

    Add Insertion Sort

--- labTestSort.py
+++ labTestSort.py
@@ -2,18 +2,27 @@ def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         swapped = False
-        # Последние i элементов уже отсортированы, их не трогаем
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
-                # Меняем элементы местами
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-        # Если обменов не было, список отсортирован
         if not swapped:
             break
     return arr
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
 numbers = [64, 34, 25, 12, 22, 11, 90]
 
 if __name__ == "__main__":
-	print("Отсортированный список:", bubble_sort(numbers))
+    print("Отсортированный список пузырьком:", bubble_sort(numbers))
+    print("Отсортированный список пузырьком:", insertion_sort(numbers))
